9a8ac05d97bcdbe778f3c8f94601ec96bbac045b
Bill Barekas <bbarekas@gmail.com>
2022-06-11 01:56:24 +0300

    Deal with IDL file.

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,7 +2,8 @@ import { computed } from 'vue'
 import { useAnchorWallet } from 'solana-wallets-vue'
 import { Connection, PublicKey } from '@solana/web3.js'
 import { AnchorProvider, Program } from '@project-serum/anchor'
-import idl from '../../../target/idl/solana_twitter.json'
+import idl from '@/idl/solana_twitter.json'
+
 
 const preflightCommitment = 'processed'
 const commitment = 'processed'
@@ -13,7 +14,9 @@ export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    //const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const connection = new Connection('https://api.testnet.solana.com', commitment)
+
     const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
